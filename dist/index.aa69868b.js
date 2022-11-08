639df54a7d531d00bae94fe0942e83a0a808d716
Ida Danielsson <idadanielsson@Idas-MBP.lan>
2022-11-08 21:33:07 +0100

    Added styling to todo list

--- dist/index.aa69868b.js
+++ dist/index.aa69868b.js
@@ -1,25 +1,42 @@
-let todos = [
-    "Promenad",
-    "Plugga",
-    "handla",
-    "Laga middag",
-    "Gymma",
-    "Tv\xe4tta", 
+class Todo {
+    constructor(todoItem){
+        this.todoItem = todoItem;
+    }
+}
+let firstTodo = new Todo("Promenad");
+let secondTodo = new Todo("Plugga");
+let thirdTodo = new Todo("Tr\xe4na");
+let fourthTodo = new Todo("Handla mat");
+let fifthTodo = new Todo("G\xf6ra matl\xe5dor");
+let todoList = [
+    firstTodo,
+    secondTodo,
+    thirdTodo,
+    fourthTodo,
+    fifthTodo
 ];
-let listContainer = document.createElement("ul");
-for(let i = 0; i < todos.length; i++){
-    let listItem = document.createElement("li");
-    let close = document.createElement("span");
-    listContainer.className = "list";
-    listItem.className = "list__item";
-    listItem.innerHTML = todos[i];
-    close.className = "list__close";
-    close.addEventListener("click", ()=>{
-        handleClick(todos[i]);
+let cart = [];
+let listContainer = document.getElementById("myList");
+let i = 0;
+for(let i1 = 0; i1 < todoList.length; i1++){
+    let item = document.createElement("li");
+    let span = document.createElement("span");
+    let checkButton = document.createElement("button");
+    item.classList.add("list__item");
+    listContainer.appendChild(item);
+    span.innerHTML = todoList[i1].todoItem;
+    span.className = "list__item__close";
+    item.appendChild(span);
+    checkButton.innerHTML = '<i class="bi bi-check-circle"></i>';
+    checkButton.classList.add("list__checkbtn");
+    item.appendChild(checkButton);
+    checkButton.addEventListener("click", ()=>{
+        handleClick(todoList[i1]);
     });
-    listContainer.appendChild(listItem);
-    listContainer.appendChild(close);
-    document.body.appendChild(listContainer);
+}
+function handleClick(Todo) {
+    console.log("Du klickade p\xe5", Todo);
+    cart.push(Todo);
 }
 
 //# sourceMappingURL=index.aa69868b.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,62 @@
-let todos = ["Promenad", "Plugga", "handla", "Laga middag", "Gymma", "Tvätta",];
 
-let listContainer = document.createElement("ul"); 
-
-for (let i = 0; i < todos.length; i++) {
-
-  let listItem = document.createElement("li"); 
-  let close = document.createElement("span");
-
-  listContainer.className = "list";
-
-  listItem.className = "list__item";
-  listItem.innerHTML = todos[i]; 
-
-  close.className = "list__close";
-  close.addEventListener ("click", () => {handleClick(todos[i])});
-  
-
-  listContainer.appendChild(listItem);
-  listContainer.appendChild(close);
-
-  document.body.appendChild(listContainer);
-
-  
+class Todo {
+    constructor(todoItem) {
+        this.todoItem = todoItem;
+        
+    }
 }
+
+let firstTodo = new Todo("Promenad");
+let secondTodo = new Todo("Plugga");
+let thirdTodo = new Todo("Träna");
+let fourthTodo = new Todo("Handla mat");
+let fifthTodo = new Todo("Göra matlådor");
+
+let todoList = [firstTodo, secondTodo, thirdTodo, fourthTodo, fifthTodo];
+
+let cart = [];
+
+let listContainer = document.getElementById("myList");
+let i = 0;
+
+for (let i = 0; i < todoList.length; i++) {
+    
+    let item = document.createElement("li");
+    let span = document.createElement("span");
+    let checkButton = document.createElement("button");
+    
+      
+    item.classList.add("list__item");
+    listContainer.appendChild(item);
+    
+    span.innerHTML= todoList[i].todoItem;
+    span.className= "list__item__close";
+    item.appendChild(span);
+    
+
+    checkButton.innerHTML = '<i class="bi bi-check-circle"></i>';
+    checkButton.classList.add("list__checkbtn");
+    item.appendChild(checkButton);
+
+    checkButton.addEventListener("click", () => {
+        handleClick(todoList[i]);
+
+});
+}
+
+function handleClick(Todo) {
+    console.log("Du klickade på", Todo);
+
+    cart.push(Todo);
+}
+
+
+
+
+
+
+
+
 
 
 
